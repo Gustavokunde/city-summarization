82de3567088b8180a897203e9b31cf2b213a06d2
gustavokunde <gustavokunde@gmail.com>
2024-02-25 12:39:31 -0300

    included google analytics config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import loadGoogleAnalytics from "./config/analyticsScript";
 import CitiesSelection from "./pages/CitiesSelection";
 import CityDetails from "./pages/CityDetails";
 
@@ -15,6 +18,14 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  useEffect(() => loadGoogleAnalytics(), []);
+
+  ReactGA.initialize(import.meta.env.VITE_APP_TRACKING_ANALYTICS_ID);
+
+  useEffect(() => {
+    //tracking users loaded the app
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <RouterProvider router={router} />
---
+++ src/config/analyticsScript.ts
@@ -0,0 +1,26 @@
+declare global {
+  interface Window {
+    dataLayer: Array<unknown>;
+  }
+}
+
+const loadGoogleAnalytics = () => {
+  const existingScript = document.getElementById("googleAnalytics");
+  if (!existingScript) {
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${
+      import.meta.env.VITE_APP_TRACKING_ANALYTICS_ID
+    }`;
+    script.id = "googleAnalytics";
+    document.body.appendChild(script);
+  }
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(arg1: string, arg2: string | Date) {
+    window.dataLayer.push(arg1);
+    window.dataLayer.push(arg2);
+  }
+  gtag("js", new Date());
+  gtag("config", import.meta.env.VITE_APP_TRACKING_ANALYTICS_ID);
+};
+export default loadGoogleAnalytics;
